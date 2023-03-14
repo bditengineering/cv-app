3e8b47a8d35224490660d31dc5305ba4af5916a5
bokykisac <dervy97@gmail.com>
2023-03-14 14:28:06 +0100

    Add ErrorBoundary

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,13 @@ import SignOut from "../components/sign_out";
 import SignIn from "../components/sign_in_form";
 import Link from "next/link";
 import createClient from "../utils/supabase_server";
+import { ErrorBoundary } from "next/dist/client/components/error-boundary";
 
 export default async function Home() {
   const supabase = createClient();
-  const { data } = await supabase.from("cv").select("*, positions(*), user: users!updated_by(*)");
+  const { data } = await supabase
+    .from("cv")
+    .select("*, positions(*), user: users!updated_by(*)");
   const {
     data: { session: supabaseSession },
   } = await supabase.auth.getSession();
@@ -17,17 +20,19 @@ export default async function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>CV App</h1>
         <div>
-          {supabaseSession ? (
-            <>
-              <SignOut />
-              <CVList cvs={data} session={supabaseSession} />
-            </>
-          ) : (
-            <>
-              <SignIn />
-              <Link href={"/signup"}>Sign Up</Link>
-            </>
-          )}
+          <ErrorBoundary fallback={<p>Something went wrong</p>}>
+            {supabaseSession ? (
+              <>
+                <SignOut />
+                <CVList cvs={data} session={supabaseSession} />
+              </>
+            ) : (
+              <>
+                <SignIn />
+                <Link href={"/signup"}>Sign Up</Link>
+              </>
+            )}
+          </ErrorBoundary>
         </div>
       </main>
     </div>
