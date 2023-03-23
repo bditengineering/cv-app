7553b291048f2dd7c4a93b236c112d2b16590d77
Stefan Ridjosic <stefan3012@hotmail.de>
2023-03-23 09:28:19 +0100

    add checkbox component

---
+++ components/ui/checkbox.tsx
@@ -0,0 +1,69 @@
+import { cva } from "class-variance-authority";
+
+interface Props {
+  checked: boolean;
+  children: React.ReactNode;
+  className?: string;
+  disabled?: boolean;
+  id?: string;
+  name: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+  supportingText?: React.ReactNode;
+}
+
+const checkboxClasses = cva(
+  [
+    "checkbox",
+    "h-5 w-5 rounded-md border-gray-300",
+    "focus:border-indigo-300 focus:ring-indigo-600",
+  ],
+  {
+    variants: {
+      disabled: {
+        true: "text-gray-300",
+        false: "text-indigo-600",
+      },
+    },
+    defaultVariants: {
+      disabled: false,
+    },
+  },
+);
+
+const Checkbox = ({
+  checked,
+  children,
+  className,
+  disabled,
+  name,
+  onChange,
+  supportingText,
+}: Props) => {
+  return (
+    <div className="flex items-start">
+      <div className="flex h-5 items-center">
+        <input
+          checked={checked}
+          className={checkboxClasses({ disabled, className })}
+          disabled={disabled}
+          id={name}
+          name={name}
+          onChange={onChange}
+          type="checkbox"
+        />
+      </div>
+
+      <div className="ml-2 text-sm leading-5">
+        <label htmlFor={name} className="font-medium text-gray-700">
+          {children}
+        </label>
+
+        {supportingText ? (
+          <p className="text-gray-600">{supportingText}</p>
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
+export default Checkbox;
