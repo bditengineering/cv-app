6c64cf593921ed11ba0265e50b8ec1793a9b4a82
Stefan Ridjosic <stefan3012@hotmail.de>
2023-03-23 13:12:25 +0100

    add Button component

---
+++ components/ui/Button.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { cva } from "class-variance-authority";
+
+interface ButtonProps {
+  children: React.ReactNode;
+  className?: string;
+  prefix?: React.ReactNode;
+  size?: "small" | "medium" | "large";
+  suffix?: React.ReactNode;
+  type?: "button" | "submit" | "reset";
+  variant?: "filled" | "tinted" | "outlined" | "plain";
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+/* TODO
+- disabled
+*/
+export const buttonClassNames = cva(
+  "button shadow rounded-lg flex items-center gap-x-2 font-semibold focus:outline-none",
+  {
+    variants: {
+      variant: {
+        filled:
+          "bg-indigo-600 hover:bg-indigo-700 text-white focus:ring-4 focus:ring-indigo-100",
+        tinted:
+          "bg-indigo-50 hover:bg-indigo-100 text-indigo-700 hover:text-indigo-800 focus:ring-4 focus:ring-indigo-100",
+        outlined:
+          "border border-gray-300 bg-gray-50 text-gray-700 hover:text-gray-800 focus:ring-4 focus:ring-gray-100",
+        plain:
+          "text-gray-600 hover:text-gray-700 hover:bg-gray-50 focus:bg-gray-50 shadow-none",
+      },
+      size: {
+        small: "py-2 px-3.5 text-sm",
+        medium: "py-2.5 px-4 text-sm",
+        large: "py-3 px-5 text-base",
+      },
+    },
+    defaultVariants: {
+      variant: "filled",
+      size: "medium",
+    },
+  },
+);
+
+const Button = ({
+  children,
+  className,
+  prefix,
+  size,
+  suffix,
+  type = "button",
+  variant,
+  onClick,
+}: ButtonProps) => {
+  return (
+    <button
+      type={type}
+      className={buttonClassNames({ className, variant, size })}
+      onClick={onClick}
+    >
+      {prefix}
+      {children}
+      {suffix}
+    </button>
+  );
+};
+
+export default Button;
