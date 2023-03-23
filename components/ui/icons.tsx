9eef705c0db04e85b7426d16d19cb377663369d0
Stefan Ridjosic <stefan3012@hotmail.de>
2023-03-23 13:05:02 +0100

    add Plus and PlusSmall icons

--- components/ui/icons.tsx
+++ components/ui/icons.tsx
@@ -1,3 +1,7 @@
+interface IconProps {
+  className?: string;
+}
+
 export const TrashCan = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -29,5 +33,35 @@ export const PlusCircle = () => (
       strokeLinejoin="round"
       d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
     />
+  </svg>
+);
+
+export const Plus = ({ className }: IconProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className={className}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M12 4.5v15m7.5-7.5h-15"
+    />
+  </svg>
+);
+
+export const PlusSmall = ({ className }: IconProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className={className}
+  >
+    <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m6-6H6" />
   </svg>
 );
