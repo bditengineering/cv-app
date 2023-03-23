22ac09f1cc9a8b3c8e8f35f996ed4713f6021b3f
Stefan Ridjosic <stefan3012@hotmail.de>
2023-03-23 13:25:36 +0100

    initial table style and structure changes

--- components/cv_list.tsx
+++ components/cv_list.tsx
@@ -1,12 +1,22 @@
 "use client";
 
 import Link from "next/link";
-import { CV } from "./types";
 import { supabase } from "../utils/supabase";
+import { cva } from "class-variance-authority";
+import { Plus } from "./Icons";
+import { buttonClassNames } from "@ui/Button";
+import type { CV } from "./types";
 
 interface CVListProps {
   cvs: CV[] | null;
 }
+
+const tableHeaderCellClasses = cva(
+  "px-6 py-3 border-0 text-gray-600 font-medium text-sm leading-normal text-left",
+);
+const tableCellClasses = cva(
+  "px-6 py-4 border-0 text-gray-600 text-base leading-normal",
+);
 
 export default function CVList({ cvs }: CVListProps) {
   async function downloadPdf(fileName: string) {
@@ -26,60 +36,70 @@ export default function CVList({ cvs }: CVListProps) {
 
   return (
     <>
-      <table className="border-3 min-w-full table-auto border-collapse my-4">
-        <thead className="justify-between">
-          <tr className="bg-gray-800">
-            <th className="px-16 py-2 text-gray-300">First Name</th>
-            <th className="px-16 py-2 text-gray-300">Last Name</th>
-            <th className="px-16 py-2 text-gray-300">Specialty</th>
-            <th className="px-16 py-2 text-gray-300">Updated At</th>
-            <th className="px-16 py-2 text-gray-300">Updated By</th>
-            <th className="px-16 py-2 text-gray-300">Download</th>
-            <th className="px-16 py-2 text-gray-300"></th>
-          </tr>
-        </thead>
-        <tbody className="bg-gray-200">
-          {cvs?.map((cv) => (
-            <tr key={cv.id}>
-              <td className="px-16 py-2 dark:text-black">{cv.first_name}</td>
-              <td className="px-16 py-2 dark:text-black">{cv.last_name}</td>
-              <td className="px-16 py-2 dark:text-black">
-                {cv.positions.title}
-              </td>
-              <td className="px-16 py-2 dark:text-black">
-                {new Date(cv.updated_at).toLocaleDateString("de-DE")}
-              </td>
-              <td className="px-16 py-2 dark:text-black">{cv.user.email}</td>
-              <td className="px-16 py-2 dark:text-black">
-                <button
-                  onClick={() =>
-                    downloadPdf(`${cv.first_name} - ${cv.positions.title}`)
-                  }
-                >
-                  Download
-                </button>
-              </td>
-              <td className="px-16 py-2 dark:text-black">
-                <Link
-                  className="rounded-md border bg-purple-700 px-4 py-2 text-white"
-                  prefetch={false}
-                  href={`/edit/${cv.id}`}
-                >
-                  Edit
-                </Link>
-              </td>
+      <div className="w-full my-4 border bg-white border-gray-200 rounded-xl overflow-hidden shadow">
+        <table className="w-full border-0">
+          <thead>
+            <tr className="bg-gray-50 border-b border-b-gray-200">
+              <th className={tableHeaderCellClasses()}>Name</th>
+              <th className={tableHeaderCellClasses()}>Role</th>
+              <th className={tableHeaderCellClasses()}>Last update</th>
+              <th
+                className={tableHeaderCellClasses({ className: "w-px" })}
+              ></th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
 
-      <Link
-        className="rounded-md border bg-purple-700 px-4 py-2 text-white block w-fit m-auto"
-        prefetch={false}
-        href={"/new"}
-      >
-        Add new CV
-      </Link>
+          <tbody>
+            {cvs?.map((cv) => (
+              <tr
+                key={cv.id}
+                className="border-b border-b-gray-200 [&:last-child]:border-b-0"
+              >
+                <td className={tableCellClasses()}>
+                  <div className="text-gray-900 font-medium">
+                    {cv.first_name} {cv.last_name}
+                  </div>
+                </td>
+                <td className={tableCellClasses()}>{cv.positions.title}</td>
+                <td className={tableCellClasses()}>
+                  <div>
+                    {new Date(cv.updated_at).toLocaleDateString("de-DE")}
+                  </div>
+
+                  <div className="truncate">{cv.user.email}</div>
+                </td>
+                <td className={tableCellClasses({ className: "w-px" })}>
+                  <div className="flex gap-3">
+                    <button
+                      className="font-semibold text-gray-600 text-base leading-normal"
+                      type="button"
+                      onClick={() =>
+                        downloadPdf(`${cv.first_name} - ${cv.positions.title}`)
+                      }
+                    >
+                      Download
+                    </button>
+
+                    <Link
+                      className="font-semibold text-indigo-700 text-base leading-normal"
+                      prefetch={false}
+                      href={`/edit/${cv.id}`}
+                    >
+                      Edit
+                    </Link>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      <div className="w-fit mx-auto">
+        <Link className={buttonClassNames()} prefetch={false} href="/new">
+          <Plus className="w-5 h-5" /> Add new CV
+        </Link>
+      </div>
     </>
   );
 }
