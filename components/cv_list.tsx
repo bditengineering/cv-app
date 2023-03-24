f56f673fcc755e5b363326eb7b30ebbaef2f84c4
Stefan Ridjosic <stefan3012@hotmail.de>
2023-03-24 13:59:47 +0100

    use table components in cv list

--- components/cv_list.tsx
+++ components/cv_list.tsx
@@ -1,22 +1,15 @@
 "use client";
 
 import Link from "next/link";
-import { cva } from "class-variance-authority";
 import { supabase } from "../utils/supabase";
 import { Plus } from "@ui/icons";
 import { buttonClasses } from "@ui/button";
+import { TableContainer, Table, TableCell, TableRow } from "@ui/table";
 import type { CV } from "./types";
 
 interface CVListProps {
   cvs: CV[] | null;
 }
-
-const tableHeaderCellClasses = cva(
-  "px-6 py-3 border-0 text-gray-600 font-medium text-sm leading-normal text-left",
-);
-const tableCellClasses = cva(
-  "px-6 py-4 border-0 text-gray-600 text-base leading-normal",
-);
 
 export default function CVList({ cvs }: CVListProps) {
   async function downloadPdf(fileName: string) {
@@ -36,39 +29,40 @@ export default function CVList({ cvs }: CVListProps) {
 
   return (
     <>
-      <div className="w-full my-4 border bg-white border-gray-200 rounded-xl overflow-hidden shadow">
-        <table className="w-full border-0">
+      <TableContainer>
+        <Table>
           <thead>
-            <tr className="bg-gray-50 border-b border-b-gray-200">
-              <th className={tableHeaderCellClasses()}>Name</th>
-              <th className={tableHeaderCellClasses()}>Role</th>
-              <th className={tableHeaderCellClasses()}>Last update</th>
-              <th
-                className={tableHeaderCellClasses({ className: "w-px" })}
-              ></th>
-            </tr>
+            <TableRow header>
+              <TableCell header align="left">
+                Name
+              </TableCell>
+              <TableCell header align="left">
+                Role
+              </TableCell>
+              <TableCell header align="left">
+                Last update
+              </TableCell>
+              <TableCell header align="left" className="w-px"></TableCell>
+            </TableRow>
           </thead>
 
           <tbody>
             {cvs?.map((cv) => (
-              <tr
-                key={cv.id}
-                className="border-b border-b-gray-200 [&:last-child]:border-b-0"
-              >
-                <td className={tableCellClasses()}>
+              <TableRow key={cv.id}>
+                <TableCell>
                   <div className="text-gray-900 font-medium">
                     {cv.first_name} {cv.last_name}
                   </div>
-                </td>
-                <td className={tableCellClasses()}>{cv.positions.title}</td>
-                <td className={tableCellClasses()}>
+                </TableCell>
+                <TableCell>{cv.positions.title}</TableCell>
+                <TableCell>
                   <div>
                     {new Date(cv.updated_at).toLocaleDateString("de-DE")}
                   </div>
 
                   <div className="truncate">{cv.user.email}</div>
-                </td>
-                <td className={tableCellClasses({ className: "w-px" })}>
+                </TableCell>
+                <TableCell className="w-px">
                   <div className="flex gap-3">
                     <button
                       className="font-semibold text-gray-600 text-base leading-normal"
@@ -88,12 +82,12 @@ export default function CVList({ cvs }: CVListProps) {
                       Edit
                     </Link>
                   </div>
-                </td>
-              </tr>
+                </TableCell>
+              </TableRow>
             ))}
           </tbody>
-        </table>
-      </div>
+        </Table>
+      </TableContainer>
 
       <div className="w-fit mx-auto">
         <Link className={buttonClasses()} prefetch={false} href="/new">
