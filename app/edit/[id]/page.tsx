7a01f5133f828c417f4265f0ee1788ed2d2ddb2e
Stefan Ridjosic <stefan3012@hotmail.de>
2023-03-27 16:19:41 +0200

    fetch, transform and pass skills to component

--- app/edit/[id]/page.tsx
+++ app/edit/[id]/page.tsx
@@ -1,4 +1,6 @@
+import { fetchSkills } from "../../../api";
 import AddNewCvForm from "../../../components/add_new_cv_form";
+import { transformSkills } from "../../../helpers";
 import styles from "../../Home.module.css";
 
 interface Props {
@@ -9,12 +11,15 @@ interface Props {
 
 export const dynamic = "force-static";
 
-export default function EditCv({ params: { id } }: Props) {
+export default async function EditCv({ params: { id } }: Props) {
+  const skillsResponse = await fetchSkills();
+  const skills = transformSkills(skillsResponse);
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className="mb-6 text-center text-5xl font-bold">Edit CV</h1>
-        <AddNewCvForm id={id} />
+        <AddNewCvForm id={id} skills={skills} />
       </main>
     </div>
   );
