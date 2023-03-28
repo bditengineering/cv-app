c9439bf5a97917ccd362258520365c561a75b5e0
Stefan Ridjosic <stefan3012@hotmail.de>
2023-03-28 16:13:53 +0200

    add className prop to container

--- components/ui/container.tsx
+++ components/ui/container.tsx
@@ -1,10 +1,15 @@
+import { cx } from "class-variance-authority";
+
 interface ContainerProps {
   children: React.ReactNode;
+  className?: string;
 }
 
-const Container = ({ children }: ContainerProps) => {
+const Container = ({ children, className }: ContainerProps) => {
   return (
-    <div className="mx-auto max-w-screen-xl py-6 px-4 sm:px-6 lg:px-8">
+    <div
+      className={cx("mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8", className)}
+    >
       {children}
     </div>
   );
