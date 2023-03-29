83a17195825baeb455a0ad025c004987ad7bdd3d
Stefan Ridjosic <stefan3012@hotmail.de>
2023-03-29 17:33:05 +0200

    change darkMode value

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ const colors = require("tailwindcss/colors");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  // this should disable auto dark mode application if that's system preference
+  // we'll change this once we properly update all components to support dark mode
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
