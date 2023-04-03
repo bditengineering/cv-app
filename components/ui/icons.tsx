dc296e21c82109f70d28b0865da36e750ced7967
Stefan Ridjosic <stefan3012@hotmail.de>
2023-04-03 11:17:47 +0200

    read props for TrashCan icon

--- components/ui/icons.tsx
+++ components/ui/icons.tsx
@@ -2,14 +2,14 @@ interface IconProps {
   className?: string;
 }
 
-export const TrashCan = () => (
+export const TrashCan = ({ className = "h-6 w-6" }: IconProps) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
     strokeWidth={1.5}
     stroke="currentColor"
-    className="h-6 w-6"
+    className={className}
   >
     <path
       strokeLinecap="round"
