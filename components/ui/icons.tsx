192911b87d0f7eb7cb2361e79df37620215f37e8
Stefan Ridjosic <stefan3012@hotmail.de>
2023-04-03 14:46:13 +0200

    add className to icon

--- components/ui/icons.tsx
+++ components/ui/icons.tsx
@@ -19,14 +19,14 @@ export const TrashCan = ({ className = "h-6 w-6" }: IconProps) => (
   </svg>
 );
 
-export const PlusCircle = () => (
+export const PlusCircle = ({ className = "h-6 w-6" }: IconProps) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
     strokeWidth={1.5}
     stroke="currentColor"
-    className="h-6 w-6"
+    className={className}
   >
     <path
       strokeLinecap="round"
