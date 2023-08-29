ef9dff634a8b22cde791986f44818bbbb035611a
bokykisac <dervy97@gmail.com>
2023-08-29 15:29:09 +0200

    update ts and disable ts config

--- next.config.js
+++ next.config.js
@@ -10,13 +10,13 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
+  // typescript: {
+  //   // !! WARN !!
+  //   // Dangerously allow production builds to successfully complete even if
+  //   // your project has type errors.
+  //   // !! WARN !!
+  //   ignoreBuildErrors: true,
+  // },
 };
 
 module.exports = nextConfig;
